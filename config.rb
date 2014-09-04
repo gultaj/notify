c1adca520644115d4ed42ce9657e81f23ca558cc
gultaj <yaschick.mail@gmail.com>
2014-09-04 12:25:36 +0300

    add images.

---
+++ config.rb
@@ -0,0 +1,13 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+#output_style = :compact
+
+#relative_assets = true
+
+#line_comments = false
+
+#preferred_syntax = :sass
